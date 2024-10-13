cc8d338b0abdfc87d1a9d04d7e6ee6615a0aeb94
surendrafed <95557837+chicken3301@users.noreply.github.com>
2024-10-13 04:57:29 -0700

    Update Asus  M2 debloat.cmd

--- Asus  M2 debloat.cmd
+++ Asus  M2 debloat.cmd
@@ -1,2 +1,23 @@
 adb shell pm uninstall --user 0 com.asus.ia.asusapp #My Asus
-
+adb shell pm uninstall -k --user 0 com.facebook.katana 
+adb shell pm uninstall -k --user 0 com.facebook.system 
+adb shell pm uninstall -k --user 0 com.facebook.appmanager
+adb shell pm uninstall -k --user 0 com.instagram.android 
+adb shell pm uninstall -k --user 0 com.facebook.orca 
+adb shell pm uninstall -k --user 0 com.asus.dm
+adb shell pm uninstall -k --user 0 com.facebook.services 
+adb shell pm uninstall -k --user 0 com.google.android.music
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.google.android.apps.tachyon
+adb shell pm uninstall -k --user 0 com.google.android.apps.wellbeing
+adb shell pm uninstall -k --user 0 com.google.android.calendar 
+adb shell pm uninstall -k --user 0 com.asus.calculator 
+adb shell pm uninstall -k --user 0 com.google.android.apps.docs 
+adb shell pm uninstall -k --user 0 com.google.android.gm
+adb shell pm uninstall -k --user 0 com.google.android.googlequicksearchbox 
+adb shell pm uninstall -k --user 0 com.google.android.apps.maps
+adb shell pm uninstall -k --user 0 com.google.android.apps.photos
+adb shell pm uninstall -k --user 0 com.google.android.videos
+adb shell pm uninstall -k --user 0 com.google.android.youtube
+adb shell pm uninstall -k --user 0 com.asus.loguploader
+pause 
